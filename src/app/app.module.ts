e467d38612083ca1a472f59a7242f5ee321dd249
Chema Muñoz <chemunoz78@gmail.com>
2023-09-09 23:59:55 +0200

    feat: add usuarios module and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
+import { UsuariosModule } from './usuarios/usuarios.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule],
+  imports: [BrowserModule, AppRoutingModule, SharedModule, UsuariosModule],
   providers: [],
   bootstrap: [AppComponent],
 })
